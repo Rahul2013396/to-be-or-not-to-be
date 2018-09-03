7d4dbc96f5ac18c88754aa262e8d942fa836f7cf
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 09:56:39 +0530

    Update lab5_q23.cpp

--- lab5_q23.cpp
+++ lab5_q23.cpp
@@ -1,13 +1,18 @@
+//including the library
 #include <iostream>
 using namespace std;
+//function
 int main(){
+	//declaring the variables
 	int a , b=1 ;
 	cout <<" what is the number ? ";
 	cin >> a;
+	//starting the loop
 	while (a>=b){
 	cout << a << endl;
 	a--;
 	}
+	//ending loop
 
 
 
