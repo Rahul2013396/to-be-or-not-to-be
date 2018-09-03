46dde1315681f7ea21d1698a8b45bc2c8034d510
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 10:00:06 +0530

    Update lab5_q4.cpp

--- lab5_q4.cpp
+++ lab5_q4.cpp
@@ -1,13 +1,18 @@
+//inclufing the library
 #include <iostream>
 using namespace std;
+//function
 int main(){
+	//declaring the variables
 	int a;
 	cout<<  "what is the number? ";
 	cin >> a;
+	//putting the conditions
 	if (a%5==0 && a%11==0 ){
 	cout << a << " is divisble by 5 and 11";
 	}
 	else {
+		//result
  	cout << a << " is not divisible by 5 and 11";
 
 	}				
