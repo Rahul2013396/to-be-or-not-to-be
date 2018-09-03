01599b85e81cf674401fc04e83c90e158ef95fd4
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 09:45:28 +0530

    Update lab5_q1.cpp

--- lab5_q1.cpp
+++ lab5_q1.cpp
@@ -1,9 +1,13 @@
+// including the library 
 #include <iostream>
 using namespace std;
+// writing the function
 int main(){
+ // declaring the variables
  float a,b ;
 cout << "what are the numbers? ";
-cin >> a >>b;  
+cin >> a >>b;
+ // putting the conditions
 if (a>b){
 cout<<  a <<"is graeter than " << b;
 }
