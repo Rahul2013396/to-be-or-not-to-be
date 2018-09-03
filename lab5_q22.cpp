8d80ab9ec1403fd53720c3d07fc80a6a4fdd1e5e
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 09:55:06 +0530

    Update lab5_q22.cpp

--- lab5_q22.cpp
+++ lab5_q22.cpp
@@ -1,13 +1,19 @@
+//including the library 
 #include <iostream>
 using namespace std;
 int main(){
+	//writing the function
 	int a , b=1;
 	cout << " what is the number? ";
 	cin >> a;
-	while (b <= a){
+	//starting the loop
+	while (b <= a)
+	// condition
+	{
 	cout << b << endl;
 	b++;
 	}
+	//loop ended
 	return 0;
 
 } 
