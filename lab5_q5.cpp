11ef8ec92a5ad16a0e1b9c453210aa8d6d25e6c3
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 10:02:07 +0530

    Update lab5_q5.cpp

--- lab5_q5.cpp
+++ lab5_q5.cpp
@@ -1,13 +1,18 @@
+//including the library
 #include <iostream>
 using namespace std;
+//writing the function
 int main(){
+	//declaring the variables
 	int a;
 	cout << " what is the number? ";
 	cin >>a ;
+	//putting the conditions
 	if (a%2==0){
 	cout << a<< " is even ";	
 	}
 	else {
+		//result
 	cout << a << " is odd";
 	}
 
