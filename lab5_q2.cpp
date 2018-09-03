bce0476a11ebb7e0acb2e2deaa02750812e8d08f
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-09-03 09:50:08 +0530

    Update lab5_q2.cpp

--- lab5_q2.cpp
+++ lab5_q2.cpp
@@ -1,9 +1,14 @@
+//including the library
 #include <iostream>
 using namespace std;
+//writing the function
 int main(){
+  //declaring variables
 int a,b,c;
+  //taking the input
 cout << "what are the numbers ";
 cin >> a>>b>>c;
+  // putting the conditions(multiple conditions)
 if (a> b && a>c){
 cout << a << " is the largest number";}
 else if (b>c && b>a){
